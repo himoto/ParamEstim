466f10eb3c0ddd39202a8976ee7c462273b22d1f
himoto <himoto@protein.osaka-u.ac.jp>
2019-07-12 20:41:47 +0900

    Change runGA option

--- runGA.jl
+++ runGA.jl
@@ -27,11 +27,11 @@ function runGA()
     searchIdx::Tuple{Array{Int64,1},Array{Int64,1}} = searchParameterIndex();
     searchRegion::Matrix{Float64} = getSearchRegion();
 
-    n_generation::Int64 = 100000;
+    n_generation::Int64 = 10000;
     n_population::Int64 = 5*size(searchRegion,2);
     n_children::Int64 = 50;
     n_gene::Int64 = size(searchRegion,2);
-    allowable_error::Float64 = 0.35;
+    allowable_error::Float64 = 0.5;
 
     (bestIndiv,bestFitness) = gaV2(
         nthParamSet,
