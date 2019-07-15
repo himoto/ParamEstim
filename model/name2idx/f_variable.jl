bbdda023cbf7ecb2eedf87a3bec25c92955a3cf2
himoto <himoto@protein.osaka-u.ac.jp>
2019-07-15 15:22:53 +0900

    Don't indent the body of the module

--- model/name2idx/f_variable.jl
+++ model/name2idx/f_variable.jl
@@ -1,48 +1,48 @@
 module V
-    
-    const F_V = [
-        "ppMEKc"
-        "CREBn"
-        "pCREBn"
-        "ERKc"
-        "ERKn"
-        "pERKc"
-        "pERKn"
-        "ppERKc"
-        "ppERKn"
-        "Elk1n"
-        "pElk1n"
-        "cFOSc"
-        "cFOSn"
-        "pcFOSc"
-        "pcFOSn"
-        "DUSPc"
-        "DUSPn"
-        "pDUSPc"
-        "pDUSPn"
-        "DUSPn_ERKn"
-        "DUSPn_pERKn"
-        "DUSPn_ppERKn"
-        "pDUSPn_ERKn"
-        "pDUSPn_pERKn"
-        "pDUSPn_ppERKn"
-        "RSKc"
-        "pRSKc"
-        "pRSKn"
-        "PrecfosmRNAn"
-        "PreduspmRNAn"
-        "cfosmRNAc"
-        "duspmRNAc"
-        "Fc"
-        "Fn"
-        "FmRNAc"
-        "PreFmRNAn"
-    ];
 
-    for (index,value) in enumerate(F_V)
-        eval(Meta.parse("const $value=$index"));
-    end
+const F_V = [
+    "ppMEKc"
+    "CREBn"
+    "pCREBn"
+    "ERKc"
+    "ERKn"
+    "pERKc"
+    "pERKn"
+    "ppERKc"
+    "ppERKn"
+    "Elk1n"
+    "pElk1n"
+    "cFOSc"
+    "cFOSn"
+    "pcFOSc"
+    "pcFOSn"
+    "DUSPc"
+    "DUSPn"
+    "pDUSPc"
+    "pDUSPn"
+    "DUSPn_ERKn"
+    "DUSPn_pERKn"
+    "DUSPn_ppERKn"
+    "pDUSPn_ERKn"
+    "pDUSPn_pERKn"
+    "pDUSPn_ppERKn"
+    "RSKc"
+    "pRSKc"
+    "pRSKn"
+    "PrecfosmRNAn"
+    "PreduspmRNAn"
+    "cfosmRNAc"
+    "duspmRNAc"
+    "Fc"
+    "Fn"
+    "FmRNAc"
+    "PreFmRNAn"
+];
 
-    const len_f_vars = length(F_V);
-
+for (index,value) in enumerate(F_V)
+    eval(Meta.parse("const $value=$index"));
 end
+
+const len_f_vars = length(F_V);
+
+end  # module
