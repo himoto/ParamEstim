db15a1c4807a96fff9658fba2fae96f9af2a0129
himoto <himoto@protein.osaka-u.ac.jp>
2020-01-18 16:04:41 +0900

    Update plot_func.jl

--- plot_func.jl
+++ plot_func.jl
@@ -1,50 +1,22 @@
-function get_ylabel(name::String)
-    if name == "Phosphorylated_MEKc"
-        return "Phosphorylated MEK\n(cytoplasm)"
-    elseif name == "Phosphorylated_ERKc"
-        return "Phosphorylated ERK\n(cytoplasm)"
-    elseif name == "Phosphorylated_RSKw"
-        return "Phosphorylated RSK\n(whole cell)"
-    elseif name == "Phosphorylated_CREBw"
-        return "Phosphorylated CREB\n(whole cell)"
-    elseif name == "dusp_mRNA"
-        return L"$\it{dusp}$"*" mRNA\nexpression"
-    elseif name == "cfos_mRNA"
-        return L"$\it{c}$"*"-"*L"$\it{fos}$"*" mRNA\nexpression"
-    elseif name == "cFos_Protein"
-        return "c-Fos Protein\nexpression"
-    elseif name == "Phosphorylated_cFos"
-        return "Phosphorylated c-Fos\nProtein expression"
-    end
-end
-
-
 function plotFunc_timecourse(Sim::Module,n_file::Int64,viz_type::String,
                                 show_all::Bool,stdev::Bool,simulations_all::Array{Float64,4})
-    if !isdir("./figure/simulation")
-        mkdir("./figure/simulation")
+    if !isdir("./figure/simulation/$viz_type")
+        mkdir("./figure/simulation/$viz_type")
     end
 
-    rc("figure",figsize = (20,8))
+    rc("figure",figsize = (4,3))
     rc("font",family = "Arial")
-    rc("mathtext",fontset = "custom")
-    rc("mathtext",it = "Arial:italic")
-    rc("font",size = 18)
-    rc("axes",linewidth = 2)
-    rc("xtick.major",width = 2)
-    rc("ytick.major",width = 2)
-    rc("xtick",direction = "in")
-    rc("ytick",direction = "in")
-    rc("lines",linewidth = 2.5)
+    rc("font",size = 20)
+    rc("axes",linewidth = 1.2)
+    rc("xtick.major",width = 1.2)
+    rc("ytick.major",width = 1.2)
+    rc("lines",linewidth = 2)
     rc("lines",markersize = 12)
 
     cmap = ["mediumblue","red"]
     shape = ["D","s"]
 
-    subplots_adjust(wspace=0.5, hspace=0.5)
-
     for (i,name) in enumerate(observables)
-        subplot(2,4,i)
         if show_all
             for j=1:n_file
                 for l in eachindex(Sim.conditions)
@@ -68,7 +40,9 @@ function plotFunc_timecourse(Sim::Module,n_file::Int64,viz_type::String,
             )
             for j=1:n_file
                 for l in eachindex(Sim.conditions)
-                    normalized[i,j,:,l] = simulations_all[i,j,:,l]./maximum(simulations_all[i,j,:,:])
+                    normalized[i,j,:,l] = (
+                        simulations_all[i,j,:,l]./maximum(simulations_all[i,j,:,:])
+                    )
                 end
             end
             for l in eachindex(Sim.conditions)
@@ -79,8 +53,14 @@ function plotFunc_timecourse(Sim::Module,n_file::Int64,viz_type::String,
             end
             if stdev
                 for l in eachindex(Sim.conditions)
-                    ymean = [mean(filter(!isnan,normalized[i,:,k,l])) for k in eachindex(Sim.t)]
-                    yerr = [std(filter(!isnan,normalized[i,:,k,l]),corrected=false) for k in eachindex(Sim.t)]
+                    ymean = [
+                        mean(filter(!isnan,normalized[i,:,k,l]))
+                        for k in eachindex(Sim.t)
+                    ]
+                    yerr = [
+                        std(filter(!isnan,normalized[i,:,k,l]),corrected=false)
+                        for k in eachindex(Sim.t)
+                    ]
                     fill_between(
                         Sim.t,ymean-yerr,ymean+yerr,
                         lw=0,color=cmap[l],alpha=0.1
@@ -124,8 +104,11 @@ function plotFunc_timecourse(Sim::Module,n_file::Int64,viz_type::String,
         yticks([0,0.3,0.6,0.9,1.2])
         ylim(0,1.2)
         xlabel("Time (min)")
-        ylabel(get_ylabel(name))
-    end
+        ylabel(replace(name, "_" => " "))
 
-    savefig("./figure/simulation/$viz_type.png",dpi=300,bbox_inches="tight")
+        savefig(
+            "./figure/simulation/$viz_type/$name.pdf", bbox_inches="tight"
+        )
+        close()
+    end
 end
