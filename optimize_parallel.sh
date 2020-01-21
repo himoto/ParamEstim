7fb21421ff3ac5fee9e9388db40602ef01ed46a6
himoto <himoto@protein.osaka-u.ac.jp>
2020-01-21 12:18:11 +0900

    Add ex.

--- optimize_parallel.sh
+++ optimize_parallel.sh
@@ -3,4 +3,12 @@
 for i in $(seq 1 10); do
     nohup julia optimize.jl $i >> logs/$i.log 2>&1 &
 done
-    
+
+# To terminate the process,
+# $ pkill -f optimize.jl
+
+<<COMMENT_OUT
+for i in $(seq 1 10); do
+    julia optimize_continue.jl $i >> logs/$i.log 2>&1 &
+done
+COMMENT_OUT
