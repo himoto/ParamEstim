7cc03a86cd4d4c11dedb9ad4a56ef6dba288a35f
himoto <himoto@protein.osaka-u.ac.jp>
2020-01-21 16:21:03 +0900

    Use fill

--- ga/initial_population.jl
+++ ga/initial_population.jl
@@ -1,25 +1,33 @@
-function get_initial_population(n_population::Int64, n_gene::Int64, search_idx::Tuple{Array{Int64,1},Array{Int64,1}},
+function get_initial_population(n_population::Int64, n_gene::Int64,
+                                search_idx::Tuple{Array{Int64,1},Array{Int64,1}},
                                 search_region::Matrix{Float64})::Matrix{Float64}
-    population::Matrix{Float64} = Inf.*ones(n_population,n_gene+1)
-    
-    println("Generating the initial population...\n")
+    println(
+        "Generating the initial population...\n"
+    )
     flush(stdout)
-
+    population::Matrix{Float64} = fill(
+        Inf, (n_population, n_gene + 1)
+    )
     for i = 1:n_population
         while isinf(population[i,end]) || isnan(population[i,end])
             for j = 1:n_gene
                 population[i,j] = rand()
             end
             population[i,end] = objective(
-                population[i,1:n_gene],search_idx,search_region
+                population[i,1:n_gene], search_idx, search_region
             )
         end
-        print(@sprintf("%d / %d\n", i, n_population))
+        print(
+            @sprintf(
+                "%d / %d\n", i, n_population
+            )
+        )
         flush(stdout)
     end
-    print("\n######\n\n")
-
-    population = sortslices(population,dims=1,by=x->x[end])
+    print(
+        "\n----------------------------------------\n\n"
+    )
+    population = sortslices(population, dims = 1, by = x->x[end])
 
     return population
 end
@@ -33,14 +41,16 @@ function get_initial_population_continue(nthParamSet::Int64, n_population::Int64
     )[1,1]
     best_indiv::Vector{Float64} = readdlm(
         @sprintf(
-            "./fitparam/%d/fit_param%d.dat",nthParamSet,generation
+            "./fitparam/%d/fit_param%d.dat", nthParamSet, generation
         )
     )[:,1]
-
-    println("Generating the initial population...\n")
+    println(
+        "Generating the initial population...\n"
+    )
     flush(stdout)
-
-    population::Matrix{Float64} = Inf.*ones(n_population,n_gene+1)
+    population::Matrix{Float64} = fill(
+        Inf, (n_population, n_gene + 1)
+    )
     for i = 1:n_population
         while isinf(population[i,end]) || isnan(population[i,end])
             for j = 1:n_gene
@@ -54,15 +64,20 @@ function get_initial_population_continue(nthParamSet::Int64, n_population::Int64
                 end
             end
             population[i,end] = objective(
-                population[i,1:n_gene],search_idx,search_region
+                population[i,1:n_gene], search_idx, search_region
             )
         end
-        print(@sprintf("%d / %d\n", i, n_population))
+        print(
+            @sprintf(
+                "%d / %d\n", i, n_population
+            )
+        )
         flush(stdout)
     end
-    print("\n----------------------------------------\n\n")
-
-    population = sortslices(population,dims=1,by=x->x[end])
+    print(
+        "\n----------------------------------------\n\n"
+    )
+    population = sortslices(population, dims = 1, by = x->x[end])
 
     return population
 end
