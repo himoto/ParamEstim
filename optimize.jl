82ce4bdb4f06fb91d18be3c2cac430cad73a1b55
himoto <himoto@protein.osaka-u.ac.jp>
2020-01-22 15:59:03 +0900

    minor update

--- optimize.jl
+++ optimize.jl
@@ -8,10 +8,14 @@ function optimize(nth_param_set::Int64)
     end
 
     try
-        files = readdir("./fitparam/$nth_param_set")
+        files = readdir(
+            "./fitparam/$nth_param_set"
+        )
         for file in files
             if occursin(".dat",file)
-                rm("./fitparam/$nth_param_set/$file")
+                rm(
+                    "./fitparam/$nth_param_set/$file"
+                )
             end
         end
     catch
@@ -22,12 +26,12 @@ function optimize(nth_param_set::Int64)
     search_region::Matrix{Float64} = get_search_region()
 
     max_generation::Int64 = 10000
-    n_population::Int64 = 5*size(search_region,2)
+    n_population::Int64 = 5*size(search_region, 2)
     n_children::Int64 = 50
     n_gene::Int64 = size(search_region,2)
     allowable_error::Float64 = 0.35
 
-    (bestIndiv,bestFitness) = ga_v2(
+    (best_indiv, best_fitness) = ga_v2(
         nth_param_set,
         max_generation,
         n_population,
@@ -36,7 +40,7 @@ function optimize(nth_param_set::Int64)
         allowable_error,
         search_idx,
         search_region
-        )
+    )
 end
 
 ###
--- optimize_continue.jl
+++ optimize_continue.jl
@@ -7,17 +7,17 @@ function optimize_continue(nth_param_set::Int64)
     search_region::Matrix{Float64} = get_search_region()
 
     max_generation::Int64 = 10000
-    n_population::Int64 = 5*size(search_region,2)
+    n_population::Int64 = 5*size(search_region, 2)
     n_children::Int64 = 50
-    n_gene::Int64 = size(search_region,2)
+    n_gene::Int64 = size(search_region, 2)
     allowable_error::Float64 = 0.35
 
-    p0_bounds = [0.1,10.0]  # [lower_bound,upper_bound]
+    p0_bounds = [0.1, 10.0]  # [lower_bound, upper_bound]
 
     if !isdir("./fitparam/$nth_param_set")
         mkdir("./fitparam/$nth_param_set")
         
-        (bestIndiv,bestFitness) = ga_v2(
+        (best_indiv, best_fitness) = ga_v2(
             nth_param_set,
             max_generation,
             n_population,
@@ -28,7 +28,7 @@ function optimize_continue(nth_param_set::Int64)
             search_region
         )
     else
-        (bestIndiv,bestFitness) = ga_v2_continue(
+        (best_indiv, best_fitness) = ga_v2_continue(
             nth_param_set,
             max_generation,
             n_population,
