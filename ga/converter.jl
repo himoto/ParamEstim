9c8c842e4ad62a447a855f29e7f219a47ac7ee55
himoto <himoto@protein.osaka-u.ac.jp>
2020-03-29 14:09:50 +0900

    Use simd

--- ga/converter.jl
+++ ga/converter.jl
@@ -2,7 +2,7 @@ function decode_gene2variable(individual_gene::Vector{Float64},
                                 search_region::Matrix{Float64})::Vector{Float64}
     indiv_var::Vector{Float64} = zeros(length(individual_gene))
     
-    for i in eachindex(individual_gene)
+    @simd for i in eachindex(individual_gene)
         @inbounds indiv_var[i] = 10^(
             individual_gene[i]*(
                 search_region[2,i] - search_region[1,i]
@@ -16,7 +16,6 @@ end
 
 function encode_bestindiv2randgene(j::Int64, best_indiv::Vector{Float64}, search_region::Matrix{Float64},
                                     p0_bounds::Vector{Float64})::Float64
-    
     rand_gene::Float64 = (
         log10(
             best_indiv[j]*10^(
