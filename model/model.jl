59541c0dd21f9ad21a55227a624beda15adb8169
himoto <himoto@protein.osaka-u.ac.jp>
2020-04-11 15:42:25 +0900

    Remove semicolon

--- model/model.jl
+++ model/model.jl
@@ -2,11 +2,11 @@ module Model
 
 export C, V, f_params, initial_values, diffeq
 
-include("name2idx/name2idx.jl");
+include("name2idx/name2idx.jl")
 using .Name2Idx
 
-include("param_const.jl");
-include("initial_condition.jl");
-include("differential_equation.jl");
+include("param_const.jl")
+include("initial_condition.jl")
+include("differential_equation.jl")
 
 end # module
