fb93817aeeb869a438eeea12c265bae95003236e
himoto <himoto@protein.osaka-u.ac.jp>
2020-05-02 14:51:08 +0900

    Create set_model.jl

--- model/model.jl
+++ model/model.jl
@@ -5,8 +5,6 @@ export C, V, f_params, initial_values, diffeq
 include("name2idx/name2idx.jl")
 using .Name2Idx
 
-include("param_const.jl")
-include("initial_condition.jl")
-include("differential_equation.jl")
+include("set_model.jl")
 
 end # module
