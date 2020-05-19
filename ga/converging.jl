25620dd3b1fad6027adb983181b439729b1b928d
himoto <31299606+himoto@users.noreply.github.com>
2020-05-19 09:55:34 +0900

    Update converging.jl

--- ga/converging.jl
+++ ga/converging.jl
@@ -27,7 +27,7 @@ function converging!(ip::Vector{Int64}, population::Matrix{Float64}, n_populatio
         population[ip[2],i] = family[ic2,i]  # Random
     end
 
-    if isinf(population[ip[2],end])
+    if !isfinite(population[ip[2],end])
         population[ip[2],end] = objective(
             population[ip[2],1:n_gene],search_idx,search_region
         )
@@ -54,18 +54,19 @@ end
 # Extended Normal Distribution Xover
 function ENDX(parents::Matrix{Float64}, n_gene::Int64)::Vector{Float64}
     child::Vector{Float64} = zeros(n_gene+1)
-    α::Float64 = sqrt((1.0-2.0*0.35^2.0))/2.0
-    β::Float64 = 0.35/sqrt(n_gene-1)
+    
+    ALPHA::Float64 = sqrt((1.0-2.0*0.35^2.0)) / 2.0
+    BETA::Float64 = 0.35 / sqrt(n_gene-1)
 
     t1::Vector{Float64} = (parents[2,1:n_gene] - parents[1,1:n_gene])./2.0
-    t2::Vector{Float64} = randn()*α*(parents[2,1:n_gene] - parents[1,1:n_gene])
+    t2::Vector{Float64} = randn()*ALPHA*(parents[2,1:n_gene] - parents[1,1:n_gene])
     t3::Vector{Float64} = zeros(n_gene)
     centroid::Vector{Float64} = reshape(
         mean(parents[3:end,1:n_gene], dims=1), n_gene
     )
 
     for i in 1:n_gene
-        t3 += randn()*β*(parents[i+2,1:n_gene] - centroid)
+        t3 += randn()*BETA*(parents[i+2,1:n_gene] - centroid)
     end
 
     for i in 1:n_gene
